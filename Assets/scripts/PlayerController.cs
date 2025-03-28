d93a7fd86bc603b0ad900298bdeefee7f9b3f520
Corin B <corin.barker@education.nsw.gov.au>
2025-03-28 15:05:29 +1100

    progress!!!!!!!!!

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -25,8 +25,9 @@ public class PlayerController : MonoBehaviour
 
         SetCountText();
 
+        winTextObject.SetActive(false);
     }
-
+ 
  void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
@@ -45,33 +46,22 @@ public class PlayerController : MonoBehaviour
  
  void OnTriggerEnter(Collider other) 
     {
- // Check if the object the player collided with has the "PickUp" tag.
  if (other.gameObject.CompareTag("PickUp")) 
         {
- // Deactivate the collided object (making it disappear).
             other.gameObject.SetActive(false);
-
- // Increment the count of "PickUp" objects collected.
-            count = count + 1;
-
- // Update the count display.
+             count = count + 1;
             SetCountText();
         }
     }
 
- // Function to update the displayed count of "PickUp" objects collected.
  void SetCountText() 
     {
- // Update the count text with the current count.
         countText.text = "Count: " + count.ToString();
 
- // Check if the count has reached or exceeded the win condition.
  if (count >= 60)
         {
- // Display the win text.
             winTextObject.SetActive(true);
 
- // Destroy the enemy GameObject.
             Destroy(GameObject.FindGameObjectWithTag("Enemy"));
         }
     }
@@ -80,10 +70,10 @@ private void OnCollisionEnter(Collision collision)
 {
  if (collision.gameObject.CompareTag("Enemy"))
     {
- // Destroy the current object
         Destroy(gameObject); 
  
- 
+        winTextObject.gameObject.SetActive(true);
+        winTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose!";
  
     }
 
